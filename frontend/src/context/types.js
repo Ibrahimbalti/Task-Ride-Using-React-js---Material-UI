fca8b039790d2752322d42d4e43b7394dc6c5e8b
Ibrahimbalti <72068885+Ibrahimbalti@users.noreply.github.com>
2022-03-13 04:40:38 -0700

    Delete types.js

--- frontend/src/context/types.js
+++
@@ -1,7 +0,0 @@
-export const SEARCH_USERS = 'SEARCH_USERS';
-export const GET_USERS = 'GEt_USERS';
-export const CLEAR_USERS = 'CLEAR_USERS';
-export const SET_LOADING = 'SET_LOADING';
-export const GET_REPOS = 'GET_REPOS';
-export const SET_ALERT = 'SET_ALERT';
-export const REMOVE_ALERT = 'REMOVE_ALERT';
